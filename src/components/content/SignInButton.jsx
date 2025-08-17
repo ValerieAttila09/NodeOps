48b8fe36406018a6c313ddabd17a227d894d9baf
ValerieAttila09 <akunvalerie76@gmail.com>
2025-08-17 22:34:30 +0700

    committed

--- src/components/content/SignInButton.jsx
+++ src/components/content/SignInButton.jsx
@@ -1,5 +1,5 @@
 export default function SignInButton() {
   return (
-    <button className="rounded-md px-4 py-[6px] border border-[#d7d7d7] bg-white shadow-xs hover:bg-[#fafafa] hover:shadow hover:border-[#d7d7d7] transition-all text-sm text-neutral-700 outfit-medium">Sign In</button>
+    <button className="rounded-full px-4 py-[6px] bg-sky-600 shadow-xs hover:bg-[#fafafa] hover:shadow hover:text-neutral-900 transition-all text-sm text-white outfit-regular">Sign In</button>
   )
 }
